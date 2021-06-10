ea97f84e53908f8131e948526b29544ec007cd99
Thammarith <8011543+thammarith@users.noreply.github.com>
2021-06-10 21:27:08 +0700

    Test

---
+++ precache-manifest.e9e939155aab7b77165219ab03b38aa1.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71372961bd314d0aa1dd9f080ab28706",
+    "url": "/index.html"
+  },
+  {
+    "revision": "639219e39c78b8a5e154",
+    "url": "/static/css/main.5c0c14f1.chunk.css"
+  },
+  {
+    "revision": "de62457e5c42e595fbe4",
+    "url": "/static/js/2.a3752ee6.chunk.js"
+  },
+  {
+    "revision": "c539cd54423426898ababe6867a114a8",
+    "url": "/static/js/2.a3752ee6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "639219e39c78b8a5e154",
+    "url": "/static/js/main.cb32320a.chunk.js"
+  },
+  {
+    "revision": "cb46feedb3ce33b82fb8",
+    "url": "/static/js/runtime-main.b7c6af5c.js"
+  },
+  {
+    "revision": "bc6057a2ee0d8af01d7c79ff04e55523",
+    "url": "/static/media/NotoSansDisplay-Black.bc6057a2.woff2"
+  },
+  {
+    "revision": "bb200946e616fa92b59e70f71f6057e2",
+    "url": "/static/media/NotoSansDisplay-BlackItalic.bb200946.woff2"
+  },
+  {
+    "revision": "b0199c00ecaac494bd02509beccee2fd",
+    "url": "/static/media/NotoSansDisplay-Bold.b0199c00.woff2"
+  },
+  {
+    "revision": "3712568968e7060faeac5883ad26078c",
+    "url": "/static/media/NotoSansDisplay-BoldItalic.37125689.woff2"
+  },
+  {
+    "revision": "d1ff1b2508ba40f62f7019076e4a6a11",
+    "url": "/static/media/NotoSansDisplay-ExtraBold.d1ff1b25.woff2"
+  },
+  {
+    "revision": "603f5535e86a351a7bef5dcf944b3f66",
+    "url": "/static/media/NotoSansDisplay-ExtraBoldItalic.603f5535.woff2"
+  },
+  {
+    "revision": "e382bbcf389f4c8caa80155a7d0c18da",
+    "url": "/static/media/NotoSansDisplay-ExtraLight.e382bbcf.woff2"
+  },
+  {
+    "revision": "0ba5c9f1b1590006e6abd7d75de4f2f2",
+    "url": "/static/media/NotoSansDisplay-ExtraLightItalic.0ba5c9f1.woff2"
+  },
+  {
+    "revision": "99317f63f27d5d9d4e6f4375768cfbe1",
+    "url": "/static/media/NotoSansDisplay-Italic.99317f63.woff2"
+  },
+  {
+    "revision": "2e0cf18c14858dd376a893a833560c4e",
+    "url": "/static/media/NotoSansDisplay-Light.2e0cf18c.woff2"
+  },
+  {
+    "revision": "02d0a57b150a10454504a8a59797f252",
+    "url": "/static/media/NotoSansDisplay-LightItalic.02d0a57b.woff2"
+  },
+  {
+    "revision": "cc8c2a25279f7ea8255f19b75291b861",
+    "url": "/static/media/NotoSansDisplay-Medium.cc8c2a25.woff2"
+  },
+  {
+    "revision": "ca42937d60b6c628acca9a400170ffe8",
+    "url": "/static/media/NotoSansDisplay-MediumItalic.ca42937d.woff2"
+  },
+  {
+    "revision": "bb731b6da2472120efe83d18eec481fe",
+    "url": "/static/media/NotoSansDisplay-Regular.bb731b6d.woff2"
+  },
+  {
+    "revision": "b7264fc3022703065dfb7f5699c92b4c",
+    "url": "/static/media/NotoSansDisplay-SemiBold.b7264fc3.woff2"
+  },
+  {
+    "revision": "00b11e5b282c7824e15e1be471b5dfdf",
+    "url": "/static/media/NotoSansDisplay-SemiBoldItalic.00b11e5b.woff2"
+  },
+  {
+    "revision": "29051e4db1b27adf8fe0da20e4b6069f",
+    "url": "/static/media/NotoSansDisplay-Thin.29051e4d.woff2"
+  },
+  {
+    "revision": "9f3b3e01e7101e5dfa8f9f039e126efe",
+    "url": "/static/media/NotoSansDisplay-ThinItalic.9f3b3e01.woff2"
+  },
+  {
+    "revision": "0fc56a7ef158a07b46a1b360542b5f2b",
+    "url": "/static/media/NotoSansThaiUI-Black.0fc56a7e.woff2"
+  },
+  {
+    "revision": "13ca9a3fac06036d45a25e2f6f2acd98",
+    "url": "/static/media/NotoSansThaiUI-Bold.13ca9a3f.woff2"
+  },
+  {
+    "revision": "df870cde107b6807621366a53b3a1dfa",
+    "url": "/static/media/NotoSansThaiUI-ExtraBold.df870cde.woff2"
+  },
+  {
+    "revision": "6bad65908abc45c1563bb4e61365c3e2",
+    "url": "/static/media/NotoSansThaiUI-ExtraLight.6bad6590.woff2"
+  },
+  {
+    "revision": "bf7a02ccc2b894a8d02d9d41a4ba218c",
+    "url": "/static/media/NotoSansThaiUI-Light.bf7a02cc.woff2"
+  },
+  {
+    "revision": "828710a4daae9ee15314d4bd90829258",
+    "url": "/static/media/NotoSansThaiUI-Medium.828710a4.woff2"
+  },
+  {
+    "revision": "5c7144b92bb6269b16725dc798d67d82",
+    "url": "/static/media/NotoSansThaiUI-Regular.5c7144b9.woff2"
+  },
+  {
+    "revision": "051806adc41f498eb73b70835fb62d75",
+    "url": "/static/media/NotoSansThaiUI-SemiBold.051806ad.woff2"
+  },
+  {
+    "revision": "abfecb05882aec866e02cb98fd6fe252",
+    "url": "/static/media/NotoSansThaiUI-Thin.abfecb05.woff2"
+  },
+  {
+    "revision": "fcb9dc947670b86af2f1e33ad251e392",
+    "url": "/static/media/data.fcb9dc94.md"
+  },
+  {
+    "revision": "d7e075bc6c134ed947b5e21f2b765897",
+    "url": "/static/media/hamburger.d7e075bc.svg"
+  },
+  {
+    "revision": "649301270c847369be7c8ad85c59ad67",
+    "url": "/static/media/lock-checked.64930127.svg"
+  },
+  {
+    "revision": "ef75887526f79ec622911e09fae0d90b",
+    "url": "/static/media/lock-unlocked.ef758875.svg"
+  },
+  {
+    "revision": "050833907c9ad2fb1e8353b636b8051c",
+    "url": "/static/media/logo.05083390.svg"
+  },
+  {
+    "revision": "72f99814172e41ec74c31159f98ed2a8",
+    "url": "/static/media/mail.72f99814.svg"
+  },
+  {
+    "revision": "8b2ff291b61bb570d958610f5c32ba2d",
+    "url": "/static/media/profile-picture.8b2ff291.svg"
+  },
+  {
+    "revision": "46308bec167cd95951343174a2557fc8",
+    "url": "/static/media/sprite.46308bec.svg"
+  },
+  {
+    "revision": "71312318cf405aae84800c3c1607f4ba",
+    "url": "/static/media/wordmark.71312318.svg"
+  }
+]);
